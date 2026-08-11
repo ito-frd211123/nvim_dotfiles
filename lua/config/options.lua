10f7f99c80461844c1a04c2649bacfdf3945ac2b
itoua <a.itou@furukawakk.co.jp>
2026-08-11 09:01:42 +0900

    modded

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- 行番号を左端に表示
+vim.opt.number = true
